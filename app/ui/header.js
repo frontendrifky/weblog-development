7f6bd3e48158aee0cc03d260ea356b9568ae5226
coderifky <coderifky1001@gmail.com>
2024-11-24 16:05:24 +0700

    set up development

---
+++ app/ui/header.js
@@ -0,0 +1,10 @@
+
+
+export default function Header(){
+    return(
+        <div className="flex justify-center border-b-2 border-yellow-400 py-2 fixed w-full">
+            <h1 className="text-2xl">Code<span className="text-yellow-500">Pedia</span></h1>
+        </div>
+            
+    )
+}
